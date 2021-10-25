67e247c621fa2a848467312c8e5b40c5acff496f
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-10-25 19:45:37 +0100

    Prepare for Deploy

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express = require('express');
+const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
-const app = express();
+const app = express()
 
 //Connect Database
-connectDB();
-
+connectDB()
 
 //Init Middlewares
-app.use(express.json({ extended: false }));
-
-
-// app.get('/', (req, res) => {
-//     return res.json({ message: 'Welcome to Backend once again'})
-// })
-
+app.use(express.json({ extended: false }))
 
 //Define our routes
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
 
-const PORT = process.env.PORT || 5000;
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'))
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
